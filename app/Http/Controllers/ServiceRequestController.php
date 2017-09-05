893389dacbf6c4ad1282167fc29ee12f369593ba
Ing. Alejandro Javier Villalón Navarro <ing.ajvillalon@gmail.com>
2017-09-05 12:43:31 -0500

    Depuración del Proyecto 9 de Septiembre 2017

---
+++ app/Http/Controllers/ServiceRequestController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace TrimaxHelp\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ServiceRequestController extends Controller
+{
+    public function mailer(){
+        
+    }
+}
